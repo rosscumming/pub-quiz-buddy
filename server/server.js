70667e8a439090c5ccadee1a5c222ac9afdb970a
Ross <rosso5792@gmail.com>
2020-03-30 18:22:56 +0100

    changes to package

--- server/server.js
+++ server/server.js
@@ -1,33 +1,29 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const path = require("path");
 const cors = require("cors");
 app.use(cors());
-
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const quizRouter = require("./helpers/quiz_router.js");
-
 app.use(bodyParser.json());
-
-MongoClient.connect("mongodb://localhost:27017")
-  .then(client => {
-    const db = client.db("quizBuddy");
-    const pubQuizzesCollection = db.collection("pubQuizzes");
-    const pubQuizzesRouter = quizRouter(pubQuizzesCollection);
-    app.use("/api/pubQuizzes/", pubQuizzesRouter);
-  })
-  .catch(console.err);
-
-app.listen(3000, function() {
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
   console.log(`listening on port ${this.address().port}`);
 });
-
-// Handle production
-if (process.env.NODE_ENV === "production") {
-  // static folder
-  app.use(express.static(__dirname + "/public"));
-
-  // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + "public/index.html"));
+//Handle production
+if (process.env.NODE_ENV == "production") {
+  MongoClient.connect(
+    "mongodb://olivia:pubquizbuddy1@ds231529.mlab.com:31529/heroku_lfzh1qkt",
+    function(err, client) {
+      if (err) {
+        console.log(err);
+      }
+      const db = client.db("heroku_lfzh1qkt");
+      const pubQuizzesCollection = db.collection("pubQuizzes");
+      const pubQuizzesRouter = quizRouter(pubQuizzesCollection);
+      app.use("/api/pubQuizzes", pubQuizzesRouter);
+      app.use("/", express.static(path.join(__dirname + "/public")));
+    }
+  );
 }
