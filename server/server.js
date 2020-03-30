f01bcdcbf8a78960037f507ddca6f6807401a470
Ross <rosso5792@gmail.com>
2020-03-30 14:38:50 +0100

    first push to heroku

--- server/server.js
+++ server/server.js
@@ -1,24 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const quizRouter = require('./helpers/quiz_router.js')
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const quizRouter = require("./helpers/quiz_router.js");
 
 app.use(bodyParser.json());
 
-MongoClient.connect('mongodb://localhost:27017')
-  .then((client) => {
-    const db = client.db('quizBuddy');
-    const pubQuizzesCollection = db.collection('pubQuizzes');
+MongoClient.connect("mongodb://localhost:27017")
+  .then(client => {
+    const db = client.db("quizBuddy");
+    const pubQuizzesCollection = db.collection("pubQuizzes");
     const pubQuizzesRouter = quizRouter(pubQuizzesCollection);
-    app.use('/api/pubQuizzes/', pubQuizzesRouter);
+    app.use("/api/pubQuizzes/", pubQuizzesRouter);
   })
   .catch(console.err);
 
-  app.listen(3000, function () {
-    console.log(`listening on port ${ this.address().port }`)
-  })
+app.listen(3000, function() {
+  console.log(`listening on port ${this.address().port}`);
+});
+
+// Handle production
+if (process.env.NODE_ENV === "production") {
+  // static folder
+  app.use(express.static(__dirname + "/public"));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "public/index.html"));
+}
